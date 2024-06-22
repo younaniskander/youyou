dd95657f0d89bf70ffd65c7b98f4fb85b7cb009e
Younan Iskander <87044703+younaniskander@users.noreply.github.com>
2024-06-22 22:43:12 +0300

    app.py

--- application.py
+++ application.py
@@ -5,60 +5,73 @@ import time
 from streamlit import caching
 from PIL import Image
 from preprocessing_images import *
+from keras.models import load_model
+import tensorflow as tf
 
-st.title('Brain MRI segmentation application')
+# Load your model
+@st.cache_resource
+def load_my_model():
+    model = load_model('model.h5')
+    return model
+
+model = load_my_model()
+
+st.title('Brain MRI Segmentation Application')
 
 st.markdown("***")
 
-
 st.subheader('Upload the MRI scan of the brain')
-option = st.radio('',('Single MRI scan', 'Multiple MRI scans'))
+option = st.radio('', ('Single MRI scan', 'Multiple MRI scans'))
 st.write('You selected:', option)
 
+def preprocess_image(image):
+    # Convert the image to the format expected by the model
+    image = image.resize((224, 224))  # Assuming the model expects 224x224 input images
+    image = np.array(image)
+    if len(image.shape) == 2:  # if grayscale, convert to RGB
+        image = np.stack((image,)*3, axis=-1)
+    image = image / 255.0  # Normalize the image
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    return image
+
+def predict(image, model):
+    preprocessed_image = preprocess_image(image)
+    prediction = model.predict(preprocessed_image)
+    return prediction
 
 if option == 'Single MRI scan':
     st.subheader('Upload the MRI scan of the brain')
-    uploaded_file = st.file_uploader(' ',accept_multiple_files = False)
+    uploaded_file = st.file_uploader(' ', accept_multiple_files=False)
 
     if uploaded_file is not None:
-        # Perform your Manupilations (In my Case applying Filters)
-        #img = load_preprocess_image(uploaded_file)
-        img = final_fun_1(uploaded_file)
-        #img = load_preprocess_image(img)
-        #st.write("Image Uploaded Successfully")
-        st.write(img)
-        img = load_preprocess_image(str(img))
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded MRI Scan', use_column_width=True)
 
-        st.image(img)
+        st.write("Classifying...")
+        prediction = predict(image, model)
+        st.write("Prediction: ", prediction)
         
     else:
-        st.write("Make sure you image is in TIF/JPG/PNG Format.")
+        st.write("Make sure your image is in TIF/JPG/PNG Format.")
 
 elif option == 'Multiple MRI scans':
     st.subheader('Upload the MRI scans of the brain')
-    uploaded_file = st.file_uploader(' ',accept_multiple_files = True)
-    if len(uploaded_file) != 0:
+    uploaded_files = st.file_uploader(' ', accept_multiple_files=True)
+    if len(uploaded_files) != 0:
         st.write("Images Uploaded Successfully")
-        # Perform your Manupilations (In my Case applying Filters)
-        for i in range(len(uploaded_file)):
-            img = final_fun_1(uploaded_file[i])
-            st.write(img)
-            img = load_preprocess_image(str(img))
+        for uploaded_file in uploaded_files:
+            image = Image.open(uploaded_file)
+            st.image(image, caption='Uploaded MRI Scan', use_column_width=True)
 
-            st.image(img)
+            st.write("Classifying...")
+            prediction = predict(image, model)
+            st.write("Prediction: ", prediction)
             
     else:
-        st.write("Make sure you image is in TIF/JPG/PNG Format.")
-
+        st.write("Make sure your images are in TIF/JPG/PNG Format.")
 
 st.markdown("***")
 
-#st.write(' Try again with different inputs')
-
-result = st.button(' Try again')
+result = st.button('Try again')
 if result:
-	
-	uploaded_file = st.empty()
-	predict_button = st.empty()
-	caching.clear_cache()
-
+    st.experimental_rerun()
