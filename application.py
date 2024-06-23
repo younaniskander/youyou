fbaf56aebce68f80330a98f19cfe3cdef06a947f
Younan Iskander <87044703+younaniskander@users.noreply.github.com>
2024-06-23 17:54:13 +0300

    application.py

--- application.py
+++ application.py
@@ -1,15 +1,17 @@
 from interface_tumor import *
 import streamlit as st
 from utils import init_session_state_variables, dataset_unzip, rename_wrong_file, check_if_dataset_exists
-from UNet_2D import init_model
 from variables import data_path
-
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+import cv2
 
 def init_app():
     """
     App Configuration
-    This functions sets & display the app title, its favicon, initialize some session_state values).
-    It also verifies that the dataset exists in the environment and well unzipped.
+    This function sets & displays the app title, its favicon, and initializes some session_state values.
+    It also verifies that the dataset exists in the environment and is well unzipped.
     """
 
     # Set config and app title
@@ -28,11 +30,63 @@ def init_app():
     # Check if the dataset exists in the environment to know if we can launch the app
     check_if_dataset_exists()
 
-    # Create & compile the CNN (U-Net model)
-    model = init_model()
+    # Load the pre-trained model
+    model = load_model('model_UNet.h5')
 
     return model
 
+def load_preprocess_image(img):
+    im = Image.open(img)
+    image = np.array(im)
+    image = image / 256.0
+    return image
+
+def predict_tumor(model, image_path):
+    """Reads a brain MRI image and returns the prediction."""
+    img = Image.open(image_path)
+    img = np.array(img)
+    img = cv2.resize(img, (256, 256))
+    img = np.array(img, dtype=np.float64)
+    img -= img.mean()
+    img /= img.std()
+    X = np.empty((1, 256, 256, 3))
+    X[0,] = img
+    prediction = model.predict(X)
+    return prediction
+
+def launch_app(model):
+    st.subheader('Upload the MRI scan of the brain')
+    option = st.radio('', ('Single MRI scan', 'Multiple MRI scans'))
+    st.write('You selected:', option)
+
+    if option == 'Single MRI scan':
+        uploaded_file = st.file_uploader(' ', accept_multiple_files=False)
+
+        if uploaded_file is not None:
+            st.image(uploaded_file)
+            prediction = predict_tumor(model, uploaded_file)
+            st.write(f"Prediction: {prediction}")
+
+        else:
+            st.write("Make sure your image is in TIF/JPG/PNG format.")
+
+    elif option == 'Multiple MRI scans':
+        uploaded_files = st.file_uploader(' ', accept_multiple_files=True)
+        if uploaded_files:
+            st.write("Images Uploaded Successfully")
+            for uploaded_file in uploaded_files:
+                st.image(uploaded_file)
+                prediction = predict_tumor(model, uploaded_file)
+                st.write(f"Prediction: {prediction}")
+
+        else:
+            st.write("Make sure your images are in TIF/JPG/PNG format.")
+
+    st.markdown("***")
+
+    result = st.button('Try again')
+    if result:
+        st.experimental_rerun()
 
 if __name__ == '__main__':
     model = init_app()
